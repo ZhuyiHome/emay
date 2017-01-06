da4fce1ebc0691888d7fca8d564174b11e9e0b24
Wade Xing <xing.xing@condenast.com.cn>
2017-01-06 12:18:30 +0800

    chore: publish to hex

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Emay.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
@@ -27,6 +29,21 @@ defmodule Emay.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:httpoison, "~> 0.10.0"},{:sweet_xml, "~> 0.6.3"}]
+    [{:httpoison, "~> 0.10.0"},{:sweet_xml, "~> 0.6.3"},{:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp package do
+    [# These are the default files included in the package
+      name: :emay,
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Wade Xing"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/ZhuyiHome/emay"}]
+  end
+  
+  defp description do
+    """
+    Send SM via Emay(亿美软通)
+    """
   end
 end

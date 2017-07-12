292d059468eb86d6ab574e32c4fe31c8f20287d3
Wang Hui Gang <weigang992003@163.com>
2017-07-12 17:25:29 +0800

    feat: put query_balance in GenServer

--- lib/emay.ex
+++ lib/emay.ex
@@ -11,8 +11,16 @@ defmodule Emay do
     {:reply, Emay.Query.to(phone, content), nil}
   end
 
+  def handle_call(:query_balance, _from, _state) do
+    {:reply, Emay.Query.query_balance, nil}
+  end
+
   # APIs
   def send(phone, content) do
     GenServer.call(Emay, {:send_to, {phone, content}})
+  end
+
+  def query_balance do
+    GenServer.call(Emay, :query_balance)
   end
 end
